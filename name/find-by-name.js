58a7c342d5e254770c04de2d90a04df209bf8e11
sarah-svg <kjskillen@gmail.com>
2020-12-01 14:08:03 -0800

    destructoring with the test

--- name/find-by-name.js
+++ name/find-by-name.js
@@ -1,11 +1,15 @@
 const dogs = require('./name');
 
 
+// const findByName = dogs[0]['name']
 
-// const findByName = dogs[0].name;
+const findByName = dogs[0].name;
 
-const findByName = dogs[0]['name']
+const { name } = dogs[0];
+
+
 
 module.exports = {
-    findByName
+     findByName,
+        name
 }
--- name/find-by-name.test.js
+++ name/find-by-name.test.js
@@ -2,15 +2,17 @@
 const  dogs  = require('./name');
 const request = require('superagent');
 
-
 const { findByName } = require('./find-by-name');
-
+const { name } = require('./find-by-name');
 
 describe('findByName', () => {
     it('grabs a dog by its name', () => {
     expected = 
       expect(findByName).toEqual('Sasha');
     });
-  
+    it('grabs a dog by its name using destruction', () => {
+      expected = 
+        expect(name).toEqual('Sasha');
+      });
   
   });
