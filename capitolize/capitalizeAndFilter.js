e795b567a2964d071c402d8204668ddf94d2b59b
sarah-svg <kjskillen@gmail.com>
2020-12-01 15:22:10 -0800

    test

--- capitolize/capitalizeAndFilter.js
+++ capitolize/capitalizeAndFilter.js
@@ -1,18 +1,25 @@
 
 const word = ["hey", "how", "are", "you", "fred"];
 
-const capitalizeAndFilter = (word) => {
-    const wordCopy = [...word];
-    let output = [];
-    for (let i = 0; i < word.length; i++) {
-        const word = wordCopy[i];
-        if (word.charAt(0) !== "f") {
-            output.push(word.toUpperCase());
-        }
-    }
-    return output;
-};
+// const capitalizeAndFilter = (word) => {
+//     const wordCopy = [...word];
+//     let output = [];
+//     for (let i = 0; i < word.length; i++) {
+//         const word = wordCopy[i];
+//         if (word.charAt(0) !== "f") {
+//             output.push(word.toUpperCase());
+//         }
+//     }
+//     return output;
+// };
 
-module.exports = { capitalizeAndFilter };
+const capitalizeAndFilter = (word) => {
+    return word
+      .filter((word) => word.charAt(0) !== "f")
+      .map((word) => word.toUpperCase());
+  };
+  
+  module.exports = { capitalizeAndFilter };
+
 
 console.log(capitalizeAndFilter(word));
