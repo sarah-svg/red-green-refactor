a608e59580e3d3bc190ec8d5cc2b57261d16038e
sarah-svg <kjskillen@gmail.com>
2020-12-01 14:26:05 -0800

    copy push with test

---
+++ copy/copy-and-push.js
@@ -0,0 +1,13 @@
+
+const pet = {
+    name: 'Rosa',
+    age: 5,
+    weight: '20 lbs'
+  };
+
+
+copyPush = { ...pet, color: 'gold'};
+module.export = {
+    copyPush
+
+}
---
+++ copy/copy.test.js
@@ -0,0 +1,17 @@
+
+const request = require('superagent');
+
+const { copyPush } = require('./copy-and-push');
+// const { name } = require('./find-by-name');
+
+describe('returns a new array with all the items and a new item', () => {
+    it('grabs the items plus a new item', () => {
+    expected = 
+      expect(copyPush).toEqual(copyPush)
+    });
+    // it('grabs a dog by its name using destruction', () => {
+    //   expected = 
+    //     expect(name).toEqual('Sasha');
+    //   });
+  
+  });
