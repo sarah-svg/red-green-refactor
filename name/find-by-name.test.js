3374b59faa2b296cad152d10b7ebb7fc96306110
sarah-svg <kjskillen@gmail.com>
2020-12-01 13:48:23 -0800

    test1 getName

--- name/find-by-name.test.js
+++ name/find-by-name.test.js
@@ -8,20 +8,9 @@ const { findByName } = require('./find-by-name');
 
 describe('findByName', () => {
     it('grabs a dog by its name', () => {
-      const white = hexToRGB('FFFFFF');
-      expect(white).toEqual({
-        r: 255,
-        g: 255,
-        b: 255
-      });
+    expected = 
+      expect(findByName).toEqual('Sasha');
     });
   
-    // it('converts a 000000 to r: 0, g: 0, b: 0', () => {
-    //   const black = hexToRGB('000000');
-    //   expect(black).toEqual({
-    //     r: 0,
-    //     g: 0,
-    //     b: 0
-    //   });
-    // });
+  
   });
