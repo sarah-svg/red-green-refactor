94eabe0c5e82d9ca608854ecc270d039580fb120
sarah-svg <kjskillen@gmail.com>
2020-12-08 19:47:32 -0800

    destructor

--- name/find-by-name.js
+++ name/find-by-name.js
@@ -1,7 +1,7 @@
-const spot = { name: "spot", age: 5, weight: "20 lbs" };
+const spot = { name: 'spot', age: 5, weight: '20 lbs' };
 
 
-const character = { _id: "5cf5679a915ecad153ab68c9", name: "Aang" };
+const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
 
 
 // const findByName = (obj) => {
@@ -16,16 +16,16 @@ const character = { _id: "5cf5679a915ecad153ab68c9", name: "Aang" };
 
 
 const findByName = (obj) => {
-  const { name } = obj
+  const { name } = obj;
   return name;
 };
 
 
-module.exports = { findByName };
+
 
 
 
 module.exports = {
-     findByName,
+  findByName,
      
-}
+};
